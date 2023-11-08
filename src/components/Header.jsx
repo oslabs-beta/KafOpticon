6a75170d849f961560278544539717b296f8d54a
AVA <ava@pop-os.localdomain>
2023-11-08 11:13:04 -0600

    png displays

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
+
 function Header() {
     return(
         <section id='headerLog'>
-            <img id='kafopticonbg' src='../../assets/kafopticonbg.png'></img>
+            <img id='kafopticonbg' src='../assets/kafopticonbg.png'></img>
             <h1 id='title'>KafOpticon</h1>
             <form>
                 <input placeholder='Cluster URL'></input>
--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ module.exports = [
             {
               loader: 'file-loader',
               options: {
-                name: '[name].[ext]', 
+                name: '[name].[hash].[ext]', 
                 outputPath: 'images', 
               },
             },
