1f83fcc2ed6ab13808dae5d3f469df3d3231edbe
AVA <ava@pop-os.localdomain>
2023-11-08 11:35:53 -0600

    position form next to logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,13 +5,15 @@ function Header() {
     return(
         <section id='headerLog'>
             <img id='kafopticonbg' src='../assets/kafopticonbg.png'></img>
-            <h1 id='title'>KafOpticon</h1>
+            <div className='formContainer'>
             <form>
                 <input placeholder='Cluster URL'></input>
                 <button id="submitbutton">Submit</button>
             </form>
+            </div>
         </section>
     )
 };
+
 
 export default Header;
