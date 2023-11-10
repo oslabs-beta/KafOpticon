34440d68bf15fb39564f96ce67fcfafd8c52a899
Joseph Paul <joepubacc@gmail.com>
2023-11-09 20:51:12 -0500

    enable dynamic appending of jmxconfig.yml

--- main/controllers/addressController.js
+++ main/controllers/addressController.js
@@ -1,30 +1,39 @@
 // this is where the controller middleware for the address route/endpoint lives
 
 const fs = require('fs').promises;
+const path = require('path');
+
+console.log(path.join(__dirname, '..', '..', 'local-test', 'scraping-config', 'jmxconfig.yml'));
+console.log(path.join('local-test', 'scraping-config'));
 
 const addressController = {};
 
 addressController.writeJmxConfig = (req, res, next) => {
   // write the jmx config file using the user inputted kafka address
   console.log('entered writeJmxConfig');
-  console.log('req.body: ', req.body);
 
+  const configFileAddress = path.join(__dirname, '..', '..', 'local-test', 'scraping-config', 'jmxconfig.yml');
   const { address } = req.body;
   // PROBABLY SHOULD SANITIZE HERE
-  console.log(address);
+
+  const lineToAppend = `hostPort: ${address}`;
+  console.log('lineToAppend:', lineToAppend);
+  
   // write to the file
-  fs.open('hello.txt', 'r+')
+  fs.open(configFileAddress, 'a')
     .then((fh) => {
-      fs.writeFile(fh, address, 'utf8')
+      fs.writeFile(fh,'\n' + lineToAppend + '\n', 'utf8')
         .then((data) => {
-          console.log('.then ~ data:', data);
+          console.log('Supposedly appended');
+          fh.close();
           next();
         })
-        .catch((err) => next({
+        .catch((err) => {
+          return next({
           log: 'An error occurred in addressController.writeJmxConfig',
           status: 422,
           message: err 
-        }))
+        })})
     })
     .catch((err) => next ({
       log: 'An error occurred in addressController.writeJmxConfig',
