dbfc7c6808b4c446daa407d0182a19389f26fe11
Joseph Paul <joepubacc@gmail.com>
2023-11-09 22:33:58 -0500

    get writeJmxConfig middleware to work properly

--- main/controllers/addressController.js
+++ main/controllers/addressController.js
@@ -1,6 +1,6 @@
 // this is where the controller middleware for the address route/endpoint lives
 
-const fs = require('fs').promises;
+const fsp = require('fs').promises;
 const path = require('path');
 
 console.log(path.join(__dirname, '..', '..', 'local-test', 'scraping-config', 'jmxconfig.yml'));
@@ -8,43 +8,45 @@ console.log(path.join('local-test', 'scraping-config'));
 
 const addressController = {};
 
-addressController.writeJmxConfig = (req, res, next) => {
+
+
+addressController.writeJmxConfig = async (req, res, next) => {
   // write the jmx config file using the user inputted kafka address
   console.log('entered writeJmxConfig');
 
-  const configFileAddress = path.join(__dirname, '..', '..', 'local-test', 'scraping-config', 'jmxconfig.yml');
+  const templateFileAddress = path.join(__dirname, '..', '..', 'local-test', 'scraping-config', 'jmxConfigTemplate.yml');
+  const destination = path.join(__dirname, '..', '..', 'local-test', 'scraping-config', 'jmxconfig.yml'); 
   const { address } = req.body;
   // PROBABLY SHOULD SANITIZE HERE
 
   const lineToAppend = `hostPort: ${address}`;
-  console.log('lineToAppend:', lineToAppend);
+
+  // a variable to store what is in the template config file
+  let newFileString = lineToAppend + '\n';
   
-  // write to the file
-  fs.open(configFileAddress, 'a')
-    .then((fh) => {
-      fs.writeFile(fh,'\n' + lineToAppend + '\n', 'utf8')
-        .then((data) => {
-          console.log('Supposedly appended');
-          fh.close();
-          next();
-        })
-        .catch((err) => {
-          return next({
-          log: 'An error occurred in addressController.writeJmxConfig',
-          status: 422,
-          message: err 
-        })})
-    })
-    .catch((err) => next ({
-      log: 'An error occurred in addressController.writeJmxConfig',
-      status: 422,
-      message: err
-    }))
+  // read the information from the template file and append it to newFileString
+  try {
+    const contents = await fsp.readFile(templateFileAddress, 'utf8');
+    newFileString += contents;
+  } catch (err) {
+    return next({chess: 'a good game'});
+  }
+
+  // write the newFileString to the destination file
+  try {
+    await fsp.writeFile(destination, newFileString, 'utf8');
+  } catch (err) {
+    return next({chess: 'a tough game'});
+  }
+
+  return next();
 
 }
 
 addressController.connectToKafka = (req, res, next) => {
   // create child process that runs jmx exporter and connect it to the kafka cluster
+
+  
   
   console.log('entered connectToKafka');
   next();
