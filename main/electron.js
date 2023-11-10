079d5ed27294a46e869a678776145c3a63f37589
AVA <ava@pop-os.localdomain>
2023-11-09 18:34:38 -0600

    render grafana graphs in electron

--- main/electron.js
+++ main/electron.js
@@ -46,8 +46,8 @@ const handleConnect = (event, data) => {
 // when electron is finished initializing and the 'ready' event is
 // emitted, boot up express server, set up ipc apis, and run createWindow
 app.on('ready', () => {
-  expressServer.listen(3000, () => {
-    console.log('Server listening on port 3000');
+  expressServer.listen(3001, () => {
+    console.log('Server listening on port 3001');
   });
   ipcMain.on('connect', handleConnect);
   createWindow();
--- src/components/MetricsContainer.jsx
+++ src/components/MetricsContainer.jsx
@@ -4,9 +4,10 @@ function MetricsContainer() {
     return (
         <section id="metricsContainer">
             <section class='metricBox'id='firstMetric'><h2>Metric 1</h2></section>
-            <section class='metricBox'id= 'secondMetric'><h2>Metric 2</h2></section>
-            <section class='metricBox'id='thirdMetric'><h2>Metric 3</h2></section>
-            <section class='metricBox'id='fourthMetric'><h2>Metric 4</h2></section>
+            <iframe src="http://localhost:3000/d-solo/fc389113-a3e9-4a69-b0c1-9fe978770298/go-dashboard?orgId=1&refresh=10s&from=1699550219475&to=1699571819475&panelId=1" width="800" height="600" frameborder="0"></iframe>
+            {/* <section class='metricBox'id= 'secondMetric'><h2>Metric 2</h2></section> */}
+            {/* <section class='metricBox'id='thirdMetric'><h2>Metric 3</h2></section> */}
+            {/* <section class='metricBox'id='fourthMetric'><h2>Metric 4</h2></section> */}
        </section>
     )
 };
