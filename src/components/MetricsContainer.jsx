71b9917ba47a4a4ac7020e9fc517951043f83edd
German Osorio <gosoriocanales@gmail.com>
2023-11-09 19:04:58 -0600

    Added grafana charts to UI

--- src/components/MetricsContainer.jsx
+++ src/components/MetricsContainer.jsx
@@ -3,10 +3,18 @@ import React from 'react';
 function MetricsContainer() {
     return (
         <section id="metricsContainer">
-            <section class='metricBox'id='firstMetric'><h2>Metric 1</h2></section>
-            <section class='metricBox'id= 'secondMetric'><h2>Metric 2</h2></section>
-            <section class='metricBox'id='thirdMetric'><h2>Metric 3</h2></section>
-            <section class='metricBox'id='fourthMetric'><h2>Metric 4</h2></section>
+            <section class='metricBox' id='firstMetric'>
+            <iframe src="http://localhost:3000/d-solo/fb4d8af9-b170-41d4-97cc-6b7011f06c6e/test-dashboard?refresh=10s&orgId=1&from=now-5m&to=now&panelId=1" width="500" height="250" frameborder="0"></iframe>
+            </section>
+            <section class='metricBox' id='secondMetric'>
+            <iframe src="http://localhost:3000/d-solo/fb4d8af9-b170-41d4-97cc-6b7011f06c6e/test-dashboard?refresh=10s&orgId=1&from=now-5m&to=now&panelId=2" width="500" height="250" frameborder="0"></iframe>
+            </section>
+            <section class='metricBox' id='thirdMetric'>
+            <iframe src="http://localhost:3000/d-solo/fb4d8af9-b170-41d4-97cc-6b7011f06c6e/test-dashboard?refresh=10s&orgId=1&from=now-5m&to=now&panelId=4" width="500" height="250" frameborder="0"></iframe>
+            </section>
+            <section class='metricBox' id='fourthMetric'>
+            <iframe src="http://localhost:3000/d-solo/fb4d8af9-b170-41d4-97cc-6b7011f06c6e/test-dashboard?refresh=10s&orgId=1&from=now-5m&to=now&panelId=5" width="500" height="250" frameborder="0"></iframe>
+            </section>
        </section>
     )
 };
