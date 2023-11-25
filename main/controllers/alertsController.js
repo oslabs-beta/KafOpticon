3883cbcbd2af5b5a32f1acb0ea0ed50d3df3fd44
Andrew <Andrew>
2023-11-25 11:54:46 -0600

    remove unneccesary code and add comments in several alerts files

--- main/controllers/alertsController.js
+++ main/controllers/alertsController.js
@@ -2,7 +2,8 @@ const path = require('path');
 const fs = require('fs')
 const ini = require('ini')
 
-
+//accepts user input to forward alerts to emails by writing information to grafana.ini config file
+//must restart grafana after to take effect
 const alertsController = {}
 
 alertsController.writeAlertsInfo = (req, res, next) => {
@@ -15,13 +16,13 @@ alertsController.writeAlertsInfo = (req, res, next) => {
     //read grafana.ini file
         const config = ini.parse(fs.readFileSync(destination, 'utf-8'))
 
-            //update grafana.ini files
+    //update grafana.ini files
         config['smtp'] = config['smtp'] || {};
         config['smtp']['user'] = emailAddress;
         config['smtp']['password'] = appPassword;
         config['smtp']['from_address'] = emailAddress;
 
-            //update grafana.ini files
+    //update grafana.ini files
         fs.writeFileSync(destination, ini.stringify(config));
     
     return next()
